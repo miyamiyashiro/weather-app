6e20245d3c1d10df78f83e53ad4d6c04515884d9
Luana Miyashiro <152427133+miyamiyashiro@users.noreply.github.com>
2026-09-17 20:09:06 -0300

    feat: build static weather app interface

---
+++ src/components/WheatherApp.jsx
@@ -0,0 +1,42 @@
+import sunny from '../assets/images/sunny.png'
+
+const WheatherApp = () => {
+  return (
+    <div className="container">
+      <div className="weather-app">
+        <div className="search">
+          <div className="search-top">
+            <i className="fa-solid fa-location-dot" aria-hidden="true"></i>
+            <div className="location">London</div>
+          </div>
+          <div className="search-bar">
+            <input type="text" placeholder="Enter Location" aria-label="Location" />
+            <i className="fa-solid fa-magnifying-glass" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div className="weather">
+          <img src={sunny} alt="Clear sky" />
+          <div className="weather-type">Clear</div>
+          <div className="temp">28°</div>
+        </div>
+        <div className="weather-date">
+          <p>Sat, 15 Ago</p>
+        </div>
+        <div className="weather-data">
+          <div className="humidity">
+            <div className="data-name">Humidity</div>
+            <i className="fa-solid fa-droplet" aria-hidden="true"></i>
+            <div className="data">35%</div>
+          </div>
+          <div className="wind">
+            <div className="data-name">Wind</div>
+            <i className="fa-solid fa-wind" aria-hidden="true"></i>
+            <div className="data">3 km/h</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WheatherApp
